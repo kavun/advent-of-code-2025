058d483e6397a67b418145aad761cda652fcfebe
Kevin Reed <662069+kavun@users.noreply.github.com>
2025-12-03 23:01:42 -0600

    d02 p02

--- d02/main.py
+++ d02/main.py
@@ -27,26 +27,41 @@ def main():
                 is1 = False
             else:
                 is1 = True
-                doubles = []
-                for double in [n for n in range(int(p1), int(p2) + 1) if is_doubled(str(n))]:
-                    part_one += double
-                    doubles.append(str(double))
-                print(f"{p1}-{p2} has {", ".join(doubles) if len(doubles) > 0 else "_"}")
+                for rep in [
+                    n for n in range(int(p1), int(p2) + 1) if is_doubled(str(n))
+                ]:
+                    part_one += rep
+
+                for rep in [
+                    n for n in range(int(p1), int(p2) + 1) if is_repeated(str(n))
+                ]:
+                    part_two += rep
+
                 p1 = ""
                 p2 = ""
                 if not char:
                     break
     print()
-    print(f"Part One: {part_one}") # 40398804950
-    print(f"Part Two: {part_two}")
+    print(f"Part One: {part_one}")  # 40398804950
+    print(f"Part Two: {part_two}")  # 65794984339
+
 
 def is_doubled(str):
-    l = len(str)
-    if (l % 2 == 0):
-        mid = l//2
-        if str[:mid] == str[mid:]:
+    ls = len(str)
+    if ls % 2 == 0:
+        if str[: ls // 2] == str[ls // 2 :]:
             return True
     return False
+
+
+def is_repeated(str):
+    ls = len(str)
+    for size in range(1, ls // 2 + 1):
+        if str[:size] * (ls // size) == str:
+            return True
+
+    return False
+
 
 if __name__ == "__main__":
     main()
